07518d106d40516e3bcc598a9422b7174cebf886
Alex <adrdra88@gmail.com>
2021-11-22 16:26:48 +0100

    tasto incremento

--- js/main.js
+++ js/main.js
@@ -57,56 +57,60 @@ const posts = [
 ];
 
 
-const {id,content,alias,media,author,name,image,likes,created} = posts;
 
-console.log(posts);
-const container = document.getElementById('container');
-let post = '';
+postIndex();
 
-
-indicePost(posts);
-function indicePost(array){
-    
-    for(let i = 0; i < array.length; i++){
-        generaPost();   
-    }
+// const {id,content,media,author,name,image,likes,created} = posts;
+function postIndex (){
+    for(let i = 0; i < posts.length; i++)
+    generatePost(posts[i]);
 }
 
-
-function generaPost(){
-    post +=
-    `   <div class="post">
-            <div class="post__header">
+function generatePost(array){
+    const container = document.getElementById('container');
+    container.innerHTML +=
+    `
+            <div class="post">
+            <div id='${array.id}' class="post__header">
                 <div class="post-meta">                    
-                <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${array.author.image}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${array.author.name}</div>
+                        <div class="post-meta__time">${array.created}</div>
+                    </div>                    
+                </div>
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
-                <div class="post-meta__time">4 mesi fa</div>
-            </div>                    
+            <div class="post__text">${array.content}</div>
+            <div class="post__image">
+                <img src="${array.media}"alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${array.id}" class="js-likes-counter">${array.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
         </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-            </div>
-        </div> 
-    </div>            
-    </div>
-    
-    `;
-    container.innerHTML = post;
 
-}
+        
+        `;
+        document.querySelector('.likes').addEventListener('click', function(event){
+            event.preventDefault(incrementButton());
+        });
+    }
+    
+function incrementButton(){
+   let incremento =  document.getElementById('like-counter-1');
+   console.log(incremento)
+   ++incremento.innerText;
+};
+
